647cbdd20fec85c809f7018cf00e9f3d4ea84b0d
zhuyang <m13426031165@163.com>
2021-12-29 18:39:20 +0800

    sss

--- hw3.py
+++ hw3.py
@@ -25,7 +25,7 @@ train_set=DatasetFolder("food-11/training/labeled",loader=lambda x:
 valid_set=DatasetFolder("food-11/validation",loader=lambda x:
                         Image.open(x),extensions="jpg",
                         transform=test_tfm)
-unlabeled_set=DatasetFolder("food-11/trainging/unlabeled",loader=lambda x:
+unlabeled_set=DatasetFolder("food-11/training/unlabeled",loader=lambda x:
                             Image.open(x),extensions="jpg",
                             transform=train_tfm)
 test_set=DatasetFolder("food-11/testing",loader=lambda x:
@@ -34,9 +34,9 @@ test_set=DatasetFolder("food-11/testing",loader=lambda x:
 
 # construct data loaders
 train_loader=DataLoader(train_set,batch_size=batch_size,
-                        shuffle=True,num_workers=2,pin_memory=True)
+                        shuffle=True,num_workers=0,pin_memory=True)
 valid_loader=DataLoader(valid_set,batch_size=batch_size,
-                        shuffle=True,num_workers=2,pin_memory=True)
+                        shuffle=True,num_workers=0,pin_memory=True)
 test_loader=DataLoader(test_set,batch_size=batch_size,shuffle=True)
 
 class Classifier(nn.Module):
@@ -66,7 +66,7 @@ class Classifier(nn.Module):
             nn.ReLU(),
             nn.Linear(256,11)
         )
-    def forward(self):
+    def forward(self,x):
         x=self.cnn_layers(x)
         x=x.flatten(1)
         x=self.fc_layers(x)
@@ -76,7 +76,7 @@ class Classifier(nn.Module):
 def get_pseudo_labels(dataset,model,threshold=0.65):
     device="cuda" if torch.cuda.is_available() else "cpu"
     data_loader=DataLoader(dataset,batch_size=batch_size,shuffle=True)
-    # make sure the model is in eval mode
+    # make sure the model is in eval mode,can turn off
     model.eval()
     # define softmax function
     softmax=nn.Softmax(dim=-1)
@@ -87,6 +87,11 @@ def get_pseudo_labels(dataset,model,threshold=0.65):
             logits=model(img.to(device))
         probs=softmax(logits)
         #TODO filter the data and construct a new dataset
+        # max_prob=probs.argmax(dim=-1)
+        max_prob=probs.topk(k=1,largest=True,dim=1)
+        max_prob>threshold
+        mask=torch.ones()
+        print(1)
 
     # turn off the eval mode
     model.train()
@@ -104,5 +109,95 @@ optimizer=torch.optim.Adam(model.parameters(),lr=0.0003,weight_decay=1e-5)
 n_epochs=10
 
 # whether to do semi-supervised learning
+do_semi=True
+
+for epoch in range(n_epochs):
+    # ---------- TODO ----------
+    # In each epoch, relabel the unlabeled dataset for semi-supervised learning.
+    # Then you can combine the labeled dataset and pseudo-labeled dataset for the training.
+    if do_semi:
+        # Obtain pseudo-labels for unlabeled data using trained model.
+        pseudo_set = get_pseudo_labels(unlabeled_set, model)
+
+        # Construct a new dataset and a data loader for training.
+        # This is used in semi-supervised learning only.
+        concat_dataset = ConcatDataset([train_set, pseudo_set])
+        train_loader = DataLoader(concat_dataset, batch_size=batch_size, shuffle=True, num_workers=0, pin_memory=True)
+
+    # ---------- Training ----------
+    # Make sure the model is in train mode before training.
+    model.train()
+
+    # These are used to record information in training.
+    train_loss = []
+    train_accs = []
+
+    # Iterate the training set by batches.
+    for batch in tqdm(train_loader):
+        # A batch consists of image data and corresponding labels.
+        imgs, labels = batch
+        # Forward the data. (Make sure data and model are on the same device.)
+        logits = model(imgs.to(device))
+        # Calculate the cross-entropy loss.
+        # We don't need to apply softmax before computing cross-entropy as it is done automatically.
+        loss = criterion(logits, labels.to(device))
+        # Gradients stored in the parameters in the previous step should be cleared out first.
+        optimizer.zero_grad()
+        # Compute the gradients for parameters.
+        loss.backward()
+        # Clip the gradient norms for stable training.使用L2范数，对模型进行剪枝。
+        grad_norm = nn.utils.clip_grad_norm_(model.parameters(), max_norm=10)
+        # Update the parameters with computed gradients.
+        optimizer.step()
+
+        # Compute the accuracy for current batch.
+        acc = (logits.argmax(dim=-1) == labels.to(device)).float().mean()
+
+        # Record the loss and accuracy.
+        train_loss.append(loss.item())
+        train_accs.append(acc)
+
+    # The average loss and accuracy of the training set is the average of the recorded values.
+    train_loss = sum(train_loss) / len(train_loss)
+    train_acc = sum(train_accs) / len(train_accs)
+
+    # Print the information.
+    print(f"[ Train | {epoch + 1:03d}/{n_epochs:03d} ] loss = {train_loss:.5f}, acc = {train_acc:.5f}")
+
+    # ---------- Validation ----------
+    # Make sure the model is in eval mode so that some modules like dropout are disabled and work normally.
+    model.eval()
+
+    # These are used to record information in validation.
+    valid_loss = []
+    valid_accs = []
+
+    # Iterate the validation set by batches.
+    for batch in tqdm(valid_loader):
+
+        # A batch consists of image data and corresponding labels.
+        imgs, labels = batch
+
+        # We don't need gradient in validation.
+        # Using torch.no_grad() accelerates the forward process.
+        with torch.no_grad():
+          logits = model(imgs.to(device))
+
+        # We can still compute the loss (but not the gradient).
+        loss = criterion(logits, labels.to(device))
+
+        # Compute the accuracy for current batch.
+        acc = (logits.argmax(dim=-1) == labels.to(device)).float().mean()
+
+        # Record the loss and accuracy.
+        valid_loss.append(loss.item())
+        valid_accs.append(acc)
+
+    # The average loss and accuracy for entire validation set is the average of the recorded values.
+    valid_loss = sum(valid_loss) / len(valid_loss)
+    valid_acc = sum(valid_accs) / len(valid_accs)
+
+    # Print the information.
+    print(f"[ Valid | {epoch + 1:03d}/{n_epochs:03d} ] loss = {valid_loss:.5f}, acc = {valid_acc:.5f}")
 
 
